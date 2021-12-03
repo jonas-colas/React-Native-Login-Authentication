909da4ebfe3b9a8176d848e8118d6e568e408785
jonascolas <jonas@fusemachines.com>
2021-12-03 11:16:03 -0300

    Login Screen UI created

--- src/screen/SignInScreen/SignInScreen.js
+++ src/screen/SignInScreen/SignInScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, useWindowDimensions } from 'react-native';
+import { View, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
 import Logo from '../../../assets/images/logoWeon.png';
 import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
@@ -35,54 +35,56 @@ const SignInScreen = () => {
   };
 
   return (
-    <View style={styles.root}>
-      <Image source={Logo} resizeMode="contain" 
-      style={[styles.logo, {height: height * 0.3}]} />
+    <ScrollView showsVerticalScrollIndicator={false} >
+      <View style={styles.root}>
+        <Image source={Logo} resizeMode="contain" 
+        style={[styles.logo, {height: height * 0.3}]} />
 
-      <CustomInput 
-        placeholder="Username" 
-        value={username} 
-        setValue={setUsername} 
-      />
-      <CustomInput 
-        placeholder="Password" 
-        value={password} 
-        setValue={setPassword} 
-        secureTextEntry 
-      />
-      
-      <CustomButton text="Sign In" 
-        onPress={onSignInPress} type="PRIMARY"/>
-      
-      <CustomButton text="Forgot Password ?" 
-        onPress={onForgotPasswordPress} type="TERTIARY" 
-      />
+        <CustomInput 
+          placeholder="Username" 
+          value={username} 
+          setValue={setUsername} 
+        />
+        <CustomInput 
+          placeholder="Password" 
+          value={password} 
+          setValue={setPassword} 
+          secureTextEntry 
+        />
+        
+        <CustomButton text="Sign In" 
+          onPress={onSignInPress} type="PRIMARY"/>
+        
+        <CustomButton text="Forgot Password ?" 
+          onPress={onForgotPasswordPress} type="TERTIARY" 
+        />
 
-      <CustomButton 
-        text="Sign In with Facebook" 
-        onPress={onSignWithFacebook} 
-        bgColor="#E7EAF4"
-        fgColor="#4765A9"
-      />
-      <CustomButton 
-        text="Sign In Google" 
-        onPress={onSignWithGoogle} 
-        bgColor="#FAE9EA"
-        fgColor="#DD4D44"
-      />
-      <CustomButton 
-        text="Sign In Apple" 
-        onPress={onSignWithApple} 
-        bgColor="#E3E3E3"
-        fgColor="#363636"
-      />
-      
-      <CustomButton 
-        text="Don't have an account? Create one" 
-        onPress={onClickSignUp} 
-        type="TERTIARY" 
-      />
-    </View>
+        <CustomButton 
+          text="Sign In with Facebook" 
+          onPress={onSignWithFacebook} 
+          bgColor="#E7EAF4"
+          fgColor="#4765A9"
+        />
+        <CustomButton 
+          text="Sign In Google" 
+          onPress={onSignWithGoogle} 
+          bgColor="#FAE9EA"
+          fgColor="#DD4D44"
+        />
+        <CustomButton 
+          text="Sign In Apple" 
+          onPress={onSignWithApple} 
+          bgColor="#E3E3E3"
+          fgColor="#363636"
+        />
+        
+        <CustomButton 
+          text="Don't have an account? Create one" 
+          onPress={onClickSignUp} 
+          type="TERTIARY" 
+        />
+      </View>
+    </ScrollView>
   );
 };
 
